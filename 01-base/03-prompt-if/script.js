05218fcb87af1b6b82ae982b4e0a885729f22515
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-08-24 15:41:47 +0200

    excercise 1

---
+++ 01-base/03-prompt-if/script.js
@@ -0,0 +1,22 @@
+/* becode/javascript
+ *
+ * /01-base/03-prompt-if/script.js - 1.3: promp & if
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function () {
+
+    var txt;
+    var answer = prompt("Do you want cake Ameerah?");
+    if (answer == "null") {
+        prompt("Congratulations");
+    } else {
+      prompt("More cake for me then :p !");
+    }
+
+})
