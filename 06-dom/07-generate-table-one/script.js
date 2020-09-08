4c35f6574f23a6176cc3e9fbddc4f0ca5c7b37e2
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-08 13:56:50 +0200

    don't know how to remove cell text without removing cell and I didn't know that i needed to use appendchild

--- 06-dom/07-generate-table-one/script.js
+++ 06-dom/07-generate-table-one/script.js
@@ -10,7 +10,43 @@
 // You will have time to focus on it later.
 
 (function() {
+   // let smallTable = table.createTHead();
+    //let row = smallTable.insertRow();
+   document.getElementById("target");
+    //var table = document.getElementById("target");
+    //var row = table.insertRow (10);
+    // get the reference for the body
+   // var body = document.getElementsByTagName("body")[0];
 
-    // your code here
+    // creates a <table> element and a <tbody> element
+    var tbl = document.createElement("table");
+    var tblBody = document.createElement("tbody");
+
+    // creating all cells
+    for (var i = 0; i < 10; i++){
+        // creates a table row
+        var row = document.createElement("tr");
+
+        for (var j = 0; j < 1; j++) {
+            // Create a <td> element and a text node, make the text
+            // node the contents of the <td>, and put the <td> at
+            // the end of the table row
+            var cell = document.createElement("td");
+           var cellText = document.createTextNode("cell in row "+i+", column "+j);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+    }
+
+    // put the <tbody> in the <table>
+    tbl.appendChild(tblBody);
+    // appends <table> into <body>
+    target.appendChild(tbl);
+    // sets the border attribute of tbl to 2;
+   // tbl.setAttribute("border", "2");
+
 
 })();
