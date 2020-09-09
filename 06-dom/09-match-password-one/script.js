ef67960e1276ebe188df8d006380b286df6c1737
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-09 09:18:44 +0200

    6.9 finished

--- 06-dom/09-match-password-one/script.js
+++ 06-dom/09-match-password-one/script.js
@@ -10,17 +10,18 @@
 // You will have time to focus on it later.
 
 (function () {
-
     document.getElementById("run").addEventListener("click", function () {
-        let password1 = document.getElementById("passe-one");
-        let password2 = document.getElementById("pass-two");
+        var password1 = document.getElementById("pass-one");
+        var password2 = document.getElementById("pass-two");
 
-        if (password1.value === password2.value) {
-            alert("ok");
-        } else {
-            //kleur borders password rood
-            password1.style.border = " 20px solid red";
-            password2.style.border = " 20px solid red";
+        if (password1.value === password2.value)
+        {
+            alert("Ok");
         }
+        else {
+            password1.style.border = "2px solid red"
+            password2.style.border = "2px solid red"
+        }
+
     })
-});
+})();
