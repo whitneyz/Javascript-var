86778a2bc296b71f781d17d268b2f921f0309b99
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-10 11:52:52 +0200

    7.1 finally finished but I think there is a easier way with less code

--- 07-misc/01-waving-text/script.js
+++ 07-misc/01-waving-text/script.js
@@ -9,8 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function () {
+    let my_span = document.createElement("span");
+    let my_span1 = document.createElement("span");
+    let my_span2 = document.createElement("span");
+    let my_span3 = document.createElement("span");
+    let my_span4 = document.createElement("span");
 
-    document.getElementById("target").style.fontSize = "larger;";
+    my_span.innerText = "Woo-hoo!";
+    document.getElementById("target").appendChild(my_span).style.fontSize = "xx-small";
 
+    my_span1.innerText = "I'm making waves!";
+    document.getElementById("target").appendChild(my_span1).style.fontSize = "small";
+
+    my_span2.innerText = "Wéééé!";
+    document.getElementById("target").appendChild(my_span2).style.fontSize = "medium";
+
+    my_span3.innerText = "Oh ...";
+    document.getElementById("target").appendChild(my_span3).style.fontSize = "large";
+
+    my_span4.innerText = "I think I'm having seasickness ...";
+    document.getElementById("target").appendChild(my_span4).style.fontSize = "xx-large";
+
+    //Woo-hoo! I'm making waves! Wéééé! Oh ...I think I'm having seasickness ...
 })();
