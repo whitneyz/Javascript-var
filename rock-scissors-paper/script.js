5dbe3e3579963ad65efb566250a8479430c47367
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-09-11 15:18:47 +0200

    scoreboard

--- rock-scissors-paper/script.js
+++ rock-scissors-paper/script.js
@@ -1,24 +1,50 @@
 (function () {
     let playersChoice;
+    let paper = 1;
+    let scissor = 2;
+    let stone = 3;
+    let count = 0;
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    let lastoutcome = 0;
+    function update () {
+        document.getElementById("playerscore").value = playerwins;
+        document.getElementById("computerscore").value = computerwins;
+    }
     document.getElementById("presspaper").addEventListener("click", function () {
-        playersChoice = "paper";
+        playersChoice = 1;
+        computer();
     })
     document.getElementById("pressscissor").addEventListener("click", function () {
-        playersChoice = "scissor";
+        playersChoice = 2;
+        computer();
     })
     document.getElementById("pressstone").addEventListener("click", function () {
-        playersChoice = "stone";
+        playersChoice = 3;
+        computer();
     })
-    document.getElementById("btncomputer").addEventListener("click", function () {
-        console.log(playersChoice); //assume that the pc picks stone
-        if (playersChoice == "paper") {
-            console.log("computer lost")
+    function computer  () {
+        console.log(playersChoice);
+        let randomChoice = Math.floor(Math.random() * 3) + 1;
+        if (playersChoice == randomChoice) {
+            console.log("draw")
         }
-        if (playersChoice == "scissor") {
-            console.log("computer won")
+        if (playersChoice === 1 && randomChoice === 2) {
+            console.log("computer won");
+           playerwins = playerwins++;
+           update ()
+        } else if (playersChoice === 2 && randomChoice === 3) {
+            console.log("computer won");
+        } else if (playersChoice === 3 && randomChoice === 1) {
+            console.log("computer won");
         }
-        if (playersChoice == "stone") {
-            console.log("it was a draw")
+        if (playersChoice === 2 && randomChoice === 1) {
+            console.log("player won");
+        } else if (playersChoice === 3 && randomChoice === 2) {
+            console.log("player won");
+        } else if (playersChoice === 1 && randomChoice ===3) {
+            console.log("player won");
         }
-    })
+    }
+
 })();
