fb859fdd868af010f0f2ea1425cbadd49771c0f6
Whitneyz <66369610+whitneyz@users.noreply.github.com>
2020-10-01 20:09:33 +0200

    changes that I didn't commit yet

--- 08-asynchronous/00-example/script.js
+++ 08-asynchronous/00-example/script.js
@@ -12,6 +12,11 @@
 
         return setInterval(() => console.log(`${label}:`, ++count), delay);
     };
+    const setupMyIntervalMessage = (delay, message) => {
+        setInterval(() => {
+            console.log(message);
+        }, delay);
+    }
 
     let intervals = [];
 
@@ -20,11 +25,20 @@
             intervals = intervals.filter(clearInterval);
             console.clear();
         }
+        /*const myFunction = () => {
+            console.log("my function has been called");
+        }*/
+        let i = 0;
+        for (i = 0; i < 3; i++) {
+            console.log("same");
+        }
 
-        intervals.push(setupIntervalMessage("one", 1000));
-        intervals.push(setupIntervalMessage("two", 1333));
-        intervals.push(setupIntervalMessage("three", 2500));
-        intervals.push(setupIntervalMessage("four", 5000));
-        intervals.push(setupIntervalMessage("five", 10000));
+        setupMyIntervalMessage(2000, "hello");
+        setupMyIntervalMessage(2500, "goodbye");
+        //intervals.push(setupIntervalMessage("one", 1000));
+        //intervals.push(setupIntervalMessage("two", 1333));
+        // intervals.push(setupIntervalMessage("three", 2500));
+        // intervals.push(setupIntervalMessage("four", 5000));
+        //intervals.push(setupIntervalMessage("five", 10000));
     });
 })();
